417ccc4e7a3d71ac5110ee6f648acf174ec99070
sylvan <sylvanlyon@gmail.com>
2025-12-31 11:48:55 +0800

    feat(cmp)!: use default keumap for completion

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,30 +1,21 @@
-local super_tab = {
-    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-    ['<C-e>'] = { 'hide', 'fallback' },
-
-    ['<Tab>'] = {
-        function(cmp)
-            if cmp.snippet_active() then
-                return cmp.accept()
-            else
-                return cmp.select_and_accept()
-            end
-        end,
-        'snippet_forward',
-        'fallback'
-    },
-    ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-
-    ['<Up>'] = { 'select_prev', 'fallback' },
-    ['<Down>'] = { 'select_next', 'fallback' },
-    ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-    ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-
-    ['<C-f>'] = { 'scroll_documentation_up', 'fallback' },
-    ['<C-b>'] = { 'scroll_documentation_down', 'fallback' },
-
-    ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
-}
+-- local keymap = {
+--     ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+--     ['<C-e>'] = { 'hide', 'fallback' },
+--     ['<C-y>'] = { 'select_and_accept', 'fallback' },
+--
+--     ['<Up>'] = { 'select_prev', 'fallback' },
+--     ['<Down>'] = { 'select_next', 'fallback' },
+--     ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+--     ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+--
+--     ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+--     ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+--
+--     ['<Tab>'] = { 'snippet_forward', 'fallback' },
+--     ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+--
+--     ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+-- }
 
 return {
     {
@@ -40,7 +31,9 @@ return {
                     nerd_font_variant = 'normal'
                 },
 
-                keymap = super_tab,
+                keymap = {
+                    preset = "default",
+                },
 
                 sources = {
                     default = { 'lsp', 'buffer', 'snippets', 'path' },
@@ -88,7 +81,9 @@ return {
                         ghost_text = { enabled = false },
                     },
 
-                    keymap = super_tab,
+                    keymap = {
+                        preset = "default",
+                    },
                 },
             })
         end,
