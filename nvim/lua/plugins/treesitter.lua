b7d548ebe5fcb2a7d2dcd124ace4c8e4625c2c80
sylvan <sylvanlyon@gmail.com>
2026-01-15 01:00:31 +0800

    feat: `TSListInstalled` user command

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -9,10 +9,22 @@ return {
             "TSUpdate",
             "TSUninstall",
             "TSLog",
+            "TSListInstalled"
         },
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter").setup()
+            vim.api.nvim_create_user_command("TSListInstalled",
+                ---@param data vim.api.keyset.create_user_command.command_args
+                function(data)
+                    local res = ""
+                    for _, lang in ipairs( require("nvim-treesitter").get_installed() ) do
+                        res = res .. lang .. "\n"
+                    end
+                    vim.print(res)
+                end,
+                { desc = "[nvim-treesitter] List all installed treesitter parsers", force = false }
+            )
         end
     },
     {
