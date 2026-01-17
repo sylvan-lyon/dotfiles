b6ae0f71389d210e8ba1a2d066b200a6ae9a643f
sylvan <sylvanlyon@gmail.com>
2026-01-17 21:58:06 +0800

    feat(wezterm, neovim): seeminglessly navigation

--- wezterm/bindings/key_binding.lua
+++ wezterm/bindings/key_binding.lua
@@ -4,32 +4,30 @@ local act = wezterm.action
 local normal_key_bindings = {
     {
         -- tmux mod for multiplexing
-        key = "`",
+        key = "b",
         mods = "CTRL",
         action = act.ActivateKeyTable({
             name = "tmux mode",
-            one_shot = false,
-            timeout_milliseconds = 800,
+            one_shot = true,
+            timeout_milliseconds = nil,
             until_unknown = false,
         })
     },
 
-    { key = "D",         mods = "CTRL|SHIFT", action = act.ShowDebugOverlay },
+    { key = "D",        mods = "CTRL|SHIFT", action = act.ShowDebugOverlay },
 
     -- 复制/粘贴
-    { key = "C",         mods = "CTRL|SHIFT", action = act.CopyTo("Clipboard") },
-    { key = "V",         mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
+    { key = "C",        mods = "CTRL|SHIFT", action = act.CopyTo("Clipboard") },
+    { key = "V",        mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
 
     -- 向上 / 向下滚动
-    { key = "UpArrow",   mods = "CTRL",       action = act.ScrollByLine(-1) },
-    { key = "DownArrow", mods = "CTRL",       action = act.ScrollByLine(1) },
-    { key = "UpArrow",   mods = "CTRL|ALT",   action = act.ScrollByPage(-0.618) },
-    { key = "DownArrow", mods = "CTRL|ALT",   action = act.ScrollByPage(0.618) },
+    { key = "PageUp",   mods = "SHIFT",      action = act.ScrollByPage(-0.618) },
+    { key = "PageDown", mods = "SHIFT",      action = act.ScrollByPage(0.618) },
 
     -- Ctrl "=" Ctrl "-" 增减字体大小, Ctrl 0 回复字体大小
-    { key = "0",         mods = "CTRL",       action = act.ResetFontSize },
-    { key = "-",         mods = "CTRL",       action = act.DecreaseFontSize },
-    { key = "=",         mods = "CTRL",       action = act.IncreaseFontSize },
+    { key = "0",        mods = "CTRL",       action = act.ResetFontSize },
+    { key = "-",        mods = "CTRL",       action = act.DecreaseFontSize },
+    { key = "=",        mods = "CTRL",       action = act.IncreaseFontSize },
 
     -- {
     --     key = "UpArrow",
@@ -88,11 +86,28 @@ local normal_key_bindings = {
     },
 }
 
+local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
+
 return {
     apply_to = function(wezterm_config)
         wezterm_config.keys = normal_key_bindings
         wezterm_config.key_tables = {
             ["tmux mode"] = require("bindings.tmux_mode")
         }
+        smart_splits.apply_to_config(wezterm_config, {
+            -- the default config is here, if you'd like to use the default keys,
+            -- you can omit this configuration table parameter and just use
+            -- smart_splits.apply_to_config(config)
+
+            -- directional keys to use in order of: left, down, up, right
+            direction_keys = { 'h', 'j', 'k', 'l' },
+            -- modifier keys to combine with direction_keys
+            modifiers = {
+                move = 'CTRL', -- modifier to use for pane movement, e.g. CTRL+h to move left
+                resize = 'ALT', -- modifier to use for pane resize, e.g. META+h to resize to the left
+            },
+            -- log level to use: info, warn, error
+            log_level = 'info',
+        })
     end
 }
--- wezterm/bindings/tmux_mode.lua
+++ wezterm/bindings/tmux_mode.lua
@@ -2,9 +2,8 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 return {
-    -- 更符合直觉的窗格分割
-    { key = "\\",     action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { key = "-",      action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "%",      action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }), mods = "SHIFT" },
+    { key = "\"",     action = act.SplitVertical({ domain = "CurrentPaneDomain" }),   mods = "SHIFT" },
     { key = "x",      action = act.CloseCurrentPane({ confirm = true }) },
     { key = "z",      action = act.TogglePaneZoomState },
 
@@ -14,33 +13,22 @@ return {
     { key = "k",      action = act.ActivatePaneDirection("Up") },
     { key = "l",      action = act.ActivatePaneDirection("Right") },
 
-    -- 调整窗格大小 (vim 风格)
-    { key = "H",      action = act.AdjustPaneSize({ "Up", 1 }),                      mods = "SHIFT" },
-    { key = "J",      action = act.AdjustPaneSize({ "Down", 1 }),                    mods = "SHIFT" },
-    { key = "H",      action = act.AdjustPaneSize({ "Left", 1 }),                    mods = "SHIFT" },
-    { key = "L",      action = act.AdjustPaneSize({ "Right", 1 }),                   mods = "SHIFT" },
+    -- -- resize panes
+    -- { key = "k",      action = act.AdjustPaneSize({ "Up", 5 }),                      mods = "ALT" },
+    -- { key = "j",      action = act.AdjustPaneSize({ "Down", 5 }),                    mods = "ALT" },
+    -- { key = "h",      action = act.AdjustPaneSize({ "Left", 5 }),                    mods = "ALT" },
+    -- { key = "l",      action = act.AdjustPaneSize({ "Right", 5 }),                   mods = "ALT" },
 
-    -- 快速调整窗格大小
-    { key = "k",      action = act.AdjustPaneSize({ "Up", 5 }),                      mods = "ALT" },
-    { key = "j",      action = act.AdjustPaneSize({ "Down", 5 }),                    mods = "ALT" },
-    { key = "h",      action = act.AdjustPaneSize({ "Left", 5 }),                    mods = "ALT" },
-    { key = "l",      action = act.AdjustPaneSize({ "Right", 5 }),                   mods = "ALT" },
-
-    -- 标签页管理
+    -- windows
     { key = "c",      action = act.SpawnTab("CurrentPaneDomain") },
     { key = "p",      action = act.ActivateTabRelative(-1) },
     { key = "n",      action = act.ActivateTabRelative(1) },
-    { key = "&",      action = act.CloseCurrentTab({ confirm = true }),              mods = "SHIFT" },
+    { key = "&",      action = act.CloseCurrentTab({ confirm = true }),               mods = "SHIFT" },
 
-    -- 标签页快捷键
-    { key = "[",      action = act.ActivateTabRelative(-1), },
-    { key = "]",      action = act.ActivateTabRelative(1), },
-
-    -- 复制
-    { key = " ",      action = act.ActivateCopyMode,                                 mods = "SHIFT" },
+    { key = "[",      action = act.ActivateCopyMode },
     { key = " ",      action = act.QuickSelect },
 
     -- 退出键表（再按一次前缀键或ESC）
-    { key = "`",      action = "PopKeyTable",                                        mods = "CTRL" },
+    { key = "b",      action = "PopKeyTable",                                         mods = "CTRL" },
     { key = "Escape", action = "PopKeyTable" },
 }
