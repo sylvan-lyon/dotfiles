b50bce960d2c12c9b848abad55e2a1059e56bbc3
sylvan <sylvanlyon@gmail.com>
2026-01-22 17:32:19 +0800

    fix(deploy): stow symlinks to wrong place

--- deploy/linux.sh
+++ deploy/linux.sh
@@ -1,4 +1,15 @@
 #! /usr/bin/env bash
-for folder in `ls` do
+if ! command -v stow > /dev/null; then
+    echo '`stow` is not installed!'
+    exit 1
+fi
+
+cd config
+echo 'depolying configuration...'
+
+for folder in `ls`
+do
     stow $folder -t $HOME
 done
+
+echo "done!"
