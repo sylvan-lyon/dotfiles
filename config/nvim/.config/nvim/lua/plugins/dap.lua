452045bf253ee5c2df720bc25809a49cb54b431f
sylvan <sylvanlyon@gmail.com>
2026-02-11 16:56:44 +0800

    refine(neovim): golang debug configuration

--- config/nvim/.config/nvim/lua/plugins/dap.lua
+++ config/nvim/.config/nvim/lua/plugins/dap.lua
@@ -10,14 +10,15 @@ local dap_setup = function()
         -- during session
         { "<leader>di", function() dap.step_into() end,         desc = "[d]ebug step [i]nto" },
         { "<leader>do", function() dap.step_over() end,         desc = "[d]ebug step [o]ver" },
-        { "<leader>dO", function() dap.step_out() end,          desc = "[d]ebug step [O]ute" },
-        { "<leader>dc", function() dap.run_to_cursor() end,     desc = "[d]ebug run to [c]ursor" },
+        { "<leader>dO", function() dap.step_out() end,          desc = "[d]ebug step [O]ut" },
+        { "<leader>dc", function() dap.continue() end,          desc = "[d]ebug [c]ontinue" },
+        { "<leader>dC", function() dap.run_to_cursor() end,     desc = "[d]ebug run to [C]ursor" },
 
         -- session manage
         { "<leader>ds", function() dap.continue() end,          desc = "[d]ebug [s]tart" },
         { "<leader>dq", function() dap.close() end,             desc = "[d]ebug [q]uit" },
         { "<leader>dQ", function() dap.terminate() end,         desc = "[d]ebug terminate" },
-        { "<leader>dr", function() dap.restart() end,           desc = "[d]ebug restart" },
+        { "<leader>dr", function() dap.restart() end,           desc = "[d]ebug [r]estart" },
 
         -- breakpoints
         { "<leader>db", function() dap.toggle_breakpoint() end, desc = "[d]ebug [b]reakpoint toggle" },
@@ -63,6 +64,7 @@ local dapui_setup = function()
     require("dapui").setup()
 
     set_keymaps({
+        { "<leader>de", function() dapui.eval() end,   desc = "[d]ebug [e]valuate", mode = { "n", "x" } },
         { "<leader>du", function() dapui.toggle() end, desc = "[d]ebug [u]i" },
     })
 end
@@ -72,7 +74,9 @@ return {
     ft = { "lua", "rust", "go" },
     dependencies = {
         { "mfussenegger/nvim-dap", config = dap_setup },
-        "theHamsta/nvim-dap-virtual-text",
+        -- { "theHamsta/nvim-dap-virtual-text", config = function ()
+        --     require("nvim-dap-virtual-text").setup({})
+        -- end },
         "nvim-neotest/nvim-nio"
     },
     config = dapui_setup
