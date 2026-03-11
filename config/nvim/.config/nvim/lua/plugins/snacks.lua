3ca465d7fab67610c26af2b86b3850b03c76fbeb
sylvan <sylvanlyon@gmail.com>
2026-03-11 20:21:40 +0800

    chore: daily update

--- config/nvim/.config/nvim/lua/plugins/snacks.lua
+++ config/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -182,8 +182,6 @@ local terminal = {
     win = {
         position = "float",
         border = "rounded",
-        width = 0.8,
-        height = 0.8,
     },
     shell = require("utils").is_windows and "nu.exe" or nil,
 }
