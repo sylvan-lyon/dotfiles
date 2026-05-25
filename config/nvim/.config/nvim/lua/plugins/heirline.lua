f80aafa62b42c99aed7c856ccc9b318cb8a51291
sylvan <sylvanlyon@gmail.com>
2026-05-26 03:12:20 +0800

    tweak(neovim): added window bar

--- config/nvim/.config/nvim/lua/plugins/heirline.lua
+++ config/nvim/.config/nvim/lua/plugins/heirline.lua
@@ -6,16 +6,15 @@ return {
     },
     config = function()
         local lib = require("plugins.config.heirline")
+        local conditions = require("heirline.conditions")
 
         require("heirline").setup({
             ---@diagnostic disable-next-line: missing-fields
             statusline = {
                 lib.right_pad(lib.mode, 1),
-                lib.right_pad(lib.file_path_block, 1),
+                lib.right_pad(lib.file_name_block, 1),
                 lib.right_pad(lib.diagnostics, 1),
-                lib.lsp,
-                -- lib.right_pad(lib.lsp_status, 0),
-                -- lib.right_pad(lib.lsp_progress, 0),
+                lib.right_pad(lib.lsp, 0),
                 lib.fill,
                 lib.macro_recording,
                 lib.fill,
@@ -29,6 +28,21 @@ return {
                 lib.left_pad(lib.scroll_bar, 1),
                 lib.left_pad(lib.ruler, 1),
                 lib.left_pad(lib.file_format, 1),
+            },
+            ---@diagnostic disable-next-line: missing-fields
+            winbar = {
+                {
+                    lib.fill,
+                    lib.buffer_type,
+                    lib.file_path_block,
+                }
+            },
+            opts = {
+                disable_winbar_cb = function(args)
+                    return conditions.buffer_matches({
+                        buftype = { "nofile", "prompt", "help", "quickfix", "term" },
+                    }, args.buf)
+                end
             },
         })
     end
